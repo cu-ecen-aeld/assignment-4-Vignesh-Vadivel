3e3a1ba8bd2e7fc938f730b2617fa1c116479f00
Vignesh-Vadivel <viva9969@colorado.edu>
2023-02-26 20:42:26 -0700

    runqemu_passes_locally

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd8d85ed1db3c94a735814ee4481d441a0ce68665'
+AESD_ASSIGNMENTS_VERSION = '6608fec88eb09eeb940cc7142c8e1f9a2c5cfd17'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
