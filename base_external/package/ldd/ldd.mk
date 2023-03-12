1dec90a5fef4f9c8141491e37a365fc0a7baa349
Vignesh-Vadivel <viva9969@colorado.edu>
2023-03-11 18:34:05 -0700

    Assignment-7-Part-2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,26 @@
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 7 git contents
+LDD_VERSION = 'b9615fb69aff7e2c574f4da493eaf1a5322c94c6'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-Vignesh-Vadivel.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = scull misc-modules
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
