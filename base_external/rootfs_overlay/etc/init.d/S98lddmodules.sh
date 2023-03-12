97416e28b484af64b8c1a9fe73111af9e551147a
Vignesh-Vadivel <viva9969@colorado.edu>
2023-03-12 13:37:33 -0600

    A-7-Make-Update

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-case "$1" in
-    start)
-         echo "starting scull and misc modules ..."
-         modprobe hello
-         /usr/bin/scull_load
-         /usr/bin/module_load faulty
-         ;;
-    stop)
-         echo "stopping scull and misc modules ..."
-         rmmod hello
-         /usr/bin/scull_unload
-         /usr/bin/module_unload faulty
-         ;;
-    *)
-
-         echo "Usage: $0 {start|stop}"
-         exit 1
-
+case "$1" in 
+	start)
+		echo 'Starting scull and misc-modules'
+		/usr/bin/module_load faulty
+		/usr/bin/scull_load
+		modprobe hello
+		;;
+	stop)
+		echo 'Stopping scull and misc-modules'
+		/usr/bin/module_unload faulty
+		/usr/bin/scull_unload
+		rmmod hello
+		;;
+	*)
+	echo "Usage: $0 {start|stop} - to load and unload kernel modules"
+	exit 1
 esac
 
 exit 0
